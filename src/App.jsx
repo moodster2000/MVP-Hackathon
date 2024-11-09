81b0be703731228379017dc000c4bf23411b1600
Aiyualive <liuaiyu1302@gmail.com>
2024-11-09 08:27:15 -0600

    make wallet connect possible

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,33 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
+import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
+import { PhantomWalletAdapter } from '@solana/wallet-adapter-wallets';
+import { clusterApiUrl } from '@solana/web3.js';
 import LandingPage from './components/LandingPage';
 import SakimonCreator from './components/SakimonCreator';
+import '@solana/wallet-adapter-react-ui/styles.css';
 
 const App = () => {
+  // You can also provide the mainnet-beta or devnet cluster
+  const endpoint = useMemo(() => clusterApiUrl('devnet'), []);
+
+  // Initialize wallet adapter
+  const wallets = useMemo(() => [], []);
+
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/create" element={<SakimonCreator />} />
-      </Routes>
-    </Router>
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallets} autoConnect>
+        <WalletModalProvider>
+          <Router>
+            <Routes>
+              <Route path="/" element={<LandingPage />} />
+              <Route path="/create" element={<SakimonCreator />} />
+            </Routes>
+          </Router>
+        </WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
   );
 };
 
